62a05d09a361e4f5968fa0741b415ceb3ef8ef25
SamarV-121 <samarvispute121@gmail.com>
2020-10-23 20:53:47 +0530

    [DNM]universal7904: Migrate all props to system
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- product_prop.mk
+++ product_prop.mk
@@ -1,13 +1,13 @@
 # Blurs
-PRODUCT_PRODUCT_PROPERTIES += \
+PRODUCT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.supports_background_blur=1
 
 # Configstore
-PRODUCT_PRODUCT_PROPERTIES += \
+PRODUCT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3
 
 # Dex2oat
-PRODUCT_PRODUCT_PROPERTIES += \
+PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat64.enabled=true \
     dalvik.vm.image-dex2oat-filter=quicken \
     dalvik.vm.image-dex2oat-threads=8 \
@@ -19,15 +19,15 @@ PRODUCT_PRODUCT_PROPERTIES += \
     dalvik.vm.boot-dex2oat-cpu-set=0,1,2,3,4,5,6,7
 
 # fastbootd
-PRODUCT_PRODUCT_PROPERTIES += \
+PRODUCT_PROPERTY_OVERRIDES += \
     ro.fastbootd.available=true
 
 # Graphics
-PRODUCT_PRODUCT_PROPERTIES += \
+PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.latch_unsignaled=1
 
 # LMKD
-PRODUCT_PRODUCT_PROPERTIES += \
+PRODUCT_PROPERTY_OVERRIDES += \
     ro.lmk.low=1001 \
     ro.lmk.medium=800 \
     ro.lmk.critical=0 \
@@ -44,11 +44,11 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.vndk.lite=false
 
 # ZRAM
-PRODUCT_PRODUCT_PROPERTIES += \
+PRODUCT_PROPERTY_OVERRIDES += \
     ro.zram.mark_idle_delay_mins=60 \
     ro.zram.first_wb_delay_mins=180 \
     ro.zram.periodic_wb_delay_hours=24
 
 # Zygote
-PRODUCT_PRODUCT_PROPERTIES += \
+PRODUCT_PROPERTY_OVERRIDES += \
     persist.device_config.runtime_native.usap_pool_enabled=true

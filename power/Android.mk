9b703256fa71fb3ea9e90f5b75db5cddb672aa9a
SamarV-121 <samarvispute121@gmail.com>
2020-12-27 15:53:35 +0530

    [TMP]Revert "universal7904: power: Install in vendor_overlay"
    This reverts commit e5e4662932baa47844523d22a10d647c3a5f0031.
    
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- power/Android.mk
+++ power/Android.mk
@@ -34,11 +34,11 @@ LOCAL_SHARED_LIBRARIES := \
     android.hardware.power@1.0 \
     vendor.lineage.power@1.0
 
-LOCAL_STATIC_LIBRARIES := libc++fs vendor.lineage.power@1.0
+LOCAL_STATIC_LIBRARIES := libc++fs
 
 LOCAL_MODULE := android.hardware.power@1.0-service.universal7904
-LOCAL_MODULE_STEM := android.hardware.power@1.0-service
-LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_EXTRA_VNDK_VERSIONS)/bin
+LOCAL_MODULE_STEM := android.hardware.power@1.0-service.exynos
+LOCAL_INIT_RC := $(LOCAL_MODULE_STEM).rc
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_OWNER := samsung
